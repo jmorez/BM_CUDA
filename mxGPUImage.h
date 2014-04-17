29589326712d75c62d0c4a84a3d87e0f4903e10d
PatronBernard <jan.morez@gmail.com>
2014-04-17 21:04:34 +0200

    Added header for mxGPUImage class

---
+++ mxGPUImage.h
@@ -0,0 +1,38 @@
+#include "gpu/mxGPUArray.h" 
+
+#ifndef MXGPUIMAGE_H
+#define MXGPUIMAGE_H
+
+/* Wrapper class with all kinds of useful methods for indexing and stuff 
+ * like that.
+ */
+class mxGPUImage {
+    public:
+        void setDataF(const mxGPUArray* );
+        float mxGPUImage::operator()(int , int);
+
+    private:
+        float* d_imagedata;
+        int M,N;
+};
+
+void mxGPUImage::setDataF(const mxGPUArray* d_array){
+    const mwSize* array_Size=mxGPUGetDimensions(d_array);
+    this->M=array_Size[0];
+    this->N=array_Size[2];
+    //This is probably wrong but it gets past compilation.
+    float* d_arrayptr=(float*)mxGPUGetDataReadOnly(d_array);
+    for (int i=0; i < M*N-1; i++){
+        d_imagedata[i]=d_arrayptr[i];
+    }
+};
+/*
+mxGPUImage createFromGPUArray(mxGPUArray* d_array){
+    mxGPUImage meh;
+    return meh;
+};
+*/
+float mxGPUImage::operator()(int i, int j){
+    return 0.;
+};
+#endif
