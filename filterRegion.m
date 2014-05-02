016b289b01b31bdbffcaf0a55674075f4e32cccc
PatronBernard <jan.morez@gmail.com>
2014-05-02 21:17:07 +0200

    Meh

--- filterRegion.m
+++ filterRegion.m
@@ -2,28 +2,28 @@ function result=filterRegion(img,i,j,region_size,searchwindow_size,blocksize,per
     
     [rows,cols]=size(img);
     
-    padding=(blocksize-1)/2;
+
     swpadding=floor((searchwindow_size-1)/2);
     
-    %Pad the image and restrict the region to/within the unpadded image.
-    img_padded=padarray(img,[padding padding]);
     
-    min_i=max(1+padding,i+padding);
-    max_i=min(rows-padding,i+region_size-1-padding);
-    min_j=max(1+padding,j+padding);
-    max_j=min(cols-padding,j+region_size-1-padding);
+    
+    %Coordinates of the ROI
+    min_i=max(1,i);
+    max_i=min(rows,i+region_size-1);
+    min_j=max(1,j);
+    max_j=min(cols,j+region_size-1);
     
     region=img(min_i:max_i,min_j:max_j);
     
     %This will be removed probably
     
-    close all
-    figure
-    imagesc(img_padded);
-    rectangle('Position',[min_j min_i (max_i-min_i) (max_j-min_j)],'EraseMode','xor')
-    axis image
-    colormap('gray')
-    
+%     close all
+%     figure
+%     imagesc(img_padded);
+%     rectangle('Position',[min_j min_i (max_i-min_i) (max_j-min_j)],'EraseMode','xor')
+%     axis image
+%     colormap('gray')
+%     
 
     tic
     %We only need the centroids for the ROI
@@ -42,24 +42,34 @@ function result=filterRegion(img,i,j,region_size,searchwindow_size,blocksize,per
     estimate_sample_size=50;
     time_estimate_array=zeros(estimate_sample_size,1);
     
+    hold on;
+    imagesc(region);
+    axis ij;
+    rectangle('Position',[min_j min_i (max_i-min_i) (max_j-min_j)],'EraseMode','xor')
+    axis image
+    colormap('gray')
+
     disp('Launching kernel...')
     for m=min_i:max_i;
         for n=min_j:max_j
             tic
+            %Seachwindow coordinates
+            sw_min_i=max(1,m-swpadding);
+            sw_max_i=min(rows,m+searchwindow_size-1);
+            sw_min_j=max(1,n-swpadding);
+            sw_max_j=min(cols,n+searchwindow_size-1);            
+            rectangle('Position',[sw_min_j sw_min_i (sw_max_j-sw_min_j) (sw_max_i-sw_min_i)]);
+            drawnow
             searchwindow=img((m-swpadding):(m+swpadding),(n-swpadding):(n+swpadding));
-            Cx_sw=Cx((m-swpadding):(m+swpadding),(n-swpadding):(n+swpadding));
-            Cy_sw=Cy((m-swpadding):(m+swpadding),(n-swpadding):(n+swpadding));
+            %Cx_sw=Cx((m-min_i+1):(m-min_i+1),(n-swpadding):(n+swpadding));
+            %Cy_sw=Cy((m-swpadding):(m+swpadding),(n-swpadding):(n+swpadding));
             
-            ref=img((m-padding):(m+padding),(n-padding):(n+padding));
-
-            similarity=findMatches(Cx_sw,Cy_sw,ref,Cx(m-i+1,n-j+1),Cy(m-i,n-j),searchwindow,mask);
-
-            similarity=findMatches(Cx,Cy,ref,region,mask);
-            matches=selectMatches(region,similarity,percentile);
-            result(m-i+1,n-j+1)=mean(matches(:));
-
-            similarity=findMatches(Cx,Cy,ref,Cx(m-i+1,n-j+1),Cy(m-i,n-j),region,mask);
-            result{m-i+1,n-j+1}=selectMatches(region,similarity,percentile);
+            %ref=img((m-padding):(m+padding),(n-padding):(n+padding));
+%           
+%             similarity=findMatches(Cx_sw,Cy_sw,ref,Cx(m-i+1,n-j+1),Cy(m-i,n-j),searchwindow,mask);
+% 
+%             matches=selectMatches(region,similarity,percentile);
+%             result(m-i+1,n-j+1)=mean(matches(:));
             
             %More waitbar/time estimation stuff
             timeleft=(max_iter-counter)*toc;
@@ -78,4 +88,5 @@ function result=filterRegion(img,i,j,region_size,searchwindow_size,blocksize,per
             counter=counter+1;
         end
     end 
+    hold off;
 end
